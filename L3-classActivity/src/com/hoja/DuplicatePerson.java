a4f71be0926e41bbe0af42e6797702491051ddba
hoja1381 <101272730+hoja1381@users.noreply.github.com>
2022-03-12 22:16:34 +0330

    Delete L3-classActivity directory

--- L3-classActivity/src/com/hoja/DuplicatePerson.java
+++
@@ -1,9 +0,0 @@
-package com.hoja;
-
-public class DuplicatePerson {
-    public void Sameperson(String a , String b) throws SamePersonEror {
-        if (a.equals(b))
-            throw new SamePersonEror();
-    }
-
-}
--- L3-classActivity/src/com/hoja/SamePersonEror.java
+++
@@ -1,6 +0,0 @@
-package com.hoja;
-public class SamePersonEror extends Exception{
-    public void SamePerson(String msg){
-        System.out.println("error same person");
-    }
-}
